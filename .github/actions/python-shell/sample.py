2aa7274f746eff1acbc650d763c73410819eaf80
Domenic Barbuzzi <dbarbuzzi@gmail.com>
2024-09-30 12:57:53 -0400

    dedupe

--- .github/actions/python-shell/sample.py
+++ .github/actions/python-shell/sample.py
@@ -4,11 +4,11 @@ import subprocess
 from pathlib import Path
 
 print("::group::shell=[default]", flush=True)
-subprocess.run("env")
+# subprocess.run("env")
 subprocess.run("env", cwd=Path.cwd())
 print("::endgroup::")
 
 print("::group::shell=True", flush=True)
-subprocess.run("env", shell=True)
+# subprocess.run("env", shell=True)
 subprocess.run("env", cwd=Path.cwd(), shell=True)
 print("::endgroup::")
