d6d19066ebe2726d5b4939f6c84dd487db1ff01d
Domenic Barbuzzi <dbarbuzzi@gmail.com>
2025-01-16 15:29:55 -0500

    Sort env vars

--- .github/actions/python-shell/sample.py
+++ .github/actions/python-shell/sample.py
@@ -4,17 +4,23 @@ import os
 import subprocess
 from pathlib import Path
 
+
+def print_env_vars(**kwargs):
+    res = subprocess.check_output("env", cwd=Path.cwd(), encoding="utf-8", **kwargs)
+    print(sorted(res.splitlines()))
+
+
 print("::group::shell=[default]", flush=True)
 # subprocess.run("env")
-subprocess.run("env", cwd=Path.cwd())
+print_env_vars()
 print("::endgroup::")
 
 print("::group::shell=True", flush=True)
 # subprocess.run("env", shell=True)
-subprocess.run("env", cwd=Path.cwd(), shell=True)
+print_env_vars(shell=True)
 print("::endgroup::")
 
 print("::group::shell=True & env=os.environ", flush=True)
 # subprocess.run("env", shell=True)
-subprocess.run("env", cwd=Path.cwd(), shell=True, env=os.environ)
+print_env_vars(shell=True, env=os.environ)
 print("::endgroup::")
